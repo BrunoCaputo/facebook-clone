479df073af507f145d1a9865155371e8f472c76f
BrunoCaputo <brunopc.dev@gmail.com>
2022-03-22 22:06:06 -0300

    feat: Added input box component and action buttons

--- components/Feed/index.js
+++ components/Feed/index.js
@@ -1,3 +1,4 @@
+import { InputBox } from "../InputBox";
 import { Stories } from "../Stories";
 
 export function Feed() {
@@ -6,7 +7,8 @@ export function Feed() {
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <Stories />
 
-        {/* Input box */}
+        <InputBox />
+
         {/* Posts */}
       </div>
     </div>
